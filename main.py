dfafad62797df5d0616b278b1921044abd5d8f38
Keiran <keiran0@proton.me>
2024-09-12 18:34:32 +0100

    update

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pyperclip
 app = typer.Typer()
 console = Console()
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://kuuichi.xyz"
 
 
 def upload_file(file_path: Path, api_key: str):
